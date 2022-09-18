92595a5bfdbb314970833357ca081364d35f0da4
Max <m@xlayer.net>
2022-09-18 15:48:06 -0400

    Fixing config

--- servermanager.sh
+++ servermanager.sh
@@ -170,7 +170,7 @@ function configureDatabase(){
 function installApache(){
 	read -p "Enter the DNS name of the server: " DOMAINNAME
 
-	sed -i "s/ServerName change.this.to.your.domain.name/ServerName $DOMAINNAME/g" "$DLUQSREPO/config/dlu.conf"
+	sed -i "s/ServerName your.domain.name/ServerName $DOMAINNAME/g" "$DLUQSREPO/config/dlu.conf"
 
 	sudo ln -s "$DLUQSREPO/config/dlu.conf"          /etc/apache2/sites-available/dlu.conf
 	sudo ln -s /etc/apache2/sites-available/dlu.conf /etc/apache2/sites-enabled/dlu.conf
